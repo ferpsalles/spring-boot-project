cb742b1bb4193aab0ad46fa333af5e46db8e45be
Fernanda Ramos <fee_salles@hotmail.com>
2021-08-24 20:12:28 -0300

    Comit inicial

---
+++ src/test/java/br/sp/gov/fatec/springbootproject/SpringBootProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package br.sp.gov.fatec.springbootproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
